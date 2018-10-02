9b44239546f7c65924d90cd08243107b9a9f63bb
Carter Carlson <carterbud1@gmail.com>
2018-10-02 16:34:40 -0500

    Begin refactorizing code to be easily readable

--- lotsofmenus.py
+++ lotsofmenus.py
@@ -1,27 +1,58 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
- 
+
 from database_setup import Restaurant, Base, MenuItem
- 
+
 engine = create_engine('sqlite:///restaurantmenu.db')
 # Bind the engine to the metadata of the Base class so that the
 # declaratives can be accessed through a DBSession instance
 Base.metadata.bind = engine
- 
+
 DBSession = sessionmaker(bind=engine)
 # A DBSession() instance establishes all conversations with the database
 # and represents a "staging zone" for all the objects loaded into the
-# database session object. Any change made against the objects in the
-# session won't be persisted into the database until you call
-# session.commit(). If you're not happy about the changes, you can
-# revert all of them back to the last commit by calling
-# session.rollback()
+# database session object.
 session = DBSession()
 
+#-------------------------------------------------------------------------------
+restaurants = ['Urban Burger',
+			   'Super Stir Fry',
+			   'Panda Garden',
+			   'Thyme for that Vegetarian Cuisine',
+			   "Tony\'s Bistro",
+			   "Andala\'s",
+			   "Auntie Anne\'s Diner",
+			   "Cocina Y Amor"]
+
+menu_items = [
+	[ # Urban Burger menu
+		['Veggie Burger', 'Juicy grilled veggie patty w/ tomato, mayo, and lettuce', '$7.50', 'Entree'],
+		['French Fries', 'with garlic and parmesan', '$2.99', 'Appetizer'],
+		['Chicken Burger', 'Juicy grilled chicken patty w/ tomato, mayo, and lettuce', '$8.00','Entree'],
+		['Chocolate Cake', 'fresh baked and served with ice cream', '$3.99', 'Dessert'],
+		['Soda', 'Your choice of Coke products', '$1.49', 'Beverage']
+	],
+	[ # Super Stir Fry menu
+		['Chicken Stir Fry', 'With your choice of noodles, veggies, and sauces', '$7.99', 'Entree'],
+		['Peking Duck', 'A famous duck dish from Beijing that has been prepared since the imperial era', '$25', 'Entree'],
+		['Beef Noodle Soup', 'A Chinese noodle soup made of stewed or braised beef, beef broth, veggies, and Chinese noodles', '$14', 'Entree'],
+		['Iced Tea', 'with Lemon', '$0.99', 'Beverage']
+	],
+	[ # Panda Garden menu
+		['Pho', 'A Vietnamese noodle soup consisting of broth, linguine-shaped rice noodles called banh pho, herbs, and meat', '$8.99', 'Entree'],
+		['Stinky Tofu', 'Taiwanese dish, deep fried fermented tofu served with pickled cabbage', '$6.99', 'Entree']
+		['Chinese Dumplings', 'A common Chinese dumpling with minced meat and finely chopped vegetables wrapped into a piece of dough skin', '$5.99', 'Appetizer'],
+		['Mochi balls', 'A small, round confection consisting of a soft, pounded sticky rice dumpling formed around an ice cream filling', '$4.99', 'Dessert']
+	],
+	[ # Time for that Vegetarian Cuisine menu
+
+	]
+]
 
 
+#-------------------------------------------------------------------------------
 #Menu for UrbanBurger
-restaurant1 = Restaurant(name = "Urban Burger")
+"restaurant1 = Restaurant(name = "Urban Burger")
 
 session.add(restaurant1)
 session.commit()
@@ -222,7 +253,7 @@ session.commit()
 
 
 
-#Menu for Andala's 
+#Menu for Andala's
 restaurant1 = Restaurant(name = "Andala\'s")
 
 session.add(restaurant1)
